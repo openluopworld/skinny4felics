970455c908e53b577126f10e67129fa2c71be189
openluopworld <luopengxq@gmail.com>
2016-12-27 20:25:53 +0800

    add implementation info

--- decrypt.c
+++ decrypt.c
@@ -1,4 +1,10 @@
 /*
+ * SKINNY-128-128
+ * @Time 2016
+ * @Author luopeng(luopeng@iie.ac.cn)
+ */
+
+/*
  *
  * University of Luxembourg
  * Laboratory of Algorithmics, Cryptology and Security (LACS)
@@ -35,70 +41,73 @@
 void Decrypt(uint8_t *block, uint8_t *roundKeys)
 {
 	/*--------------------------------------*/
-	/* r6-r7	: temp use					*/
-	/* r8-r23	: cipher text				*/
-	/* r24 		: loop control				*/
-	/* r25		: const 0x02				*/
-	/* r26-r27	: X points to plain text	*/
-	/* r30-r31	: Z points to roundKeys		*/
+	/* r6-r7    : temp use                  */
+	/* r8-r23   : cipher text               */
+	/* r24      : loop control              */
+	/* r25      : const 0x02                */
+	/* r26-r27  : X points to plain text    */
+	/* r30-r31  : Z points to roundKeys     */
 	/* -------------------------------------*/
 	// s0  s1  s2  s3       r8  r9  r10 r11
 	// s4  s5  s6  s7   =   r12 r13 r14 r15
 	// s8  s9  s10 s11  =   r16 r17 r18 r19
 	// s12 s13 s14 s15      r20 r21 r22 r23
-    asm volatile(
+	asm volatile(
 	/*
 	 * http://www.atmel.com/webdoc/AVRLibcReferenceManual/FAQ_1faq_reg_usage.html
 	 * 
-	 * GCC AVR passes arguments from left to right in r25-r8. All arguments are aligned to start in even-numbered registers. 
-	 * 			Pointers are 16-bits, so arguments are in r25:r24 and r23:22
-	 * [r18-r27, r30-r31]:	You may use them freely in assembler subroutines. The caller is responsible for saving and restoring.
-	 * [r2-r17, r28-r29]:	Calling C subroutines leaves them unchanged. Assembler subroutines are responsible for saving and restoring these 
-	 * 			registers
-	 * [r0, r1]:		Fixed registers. Never allocated by gcc for local data.
+	 * GCC AVR passes arguments from left to right in r25-r8.
+	 * All arguments are aligned to start in even-numbered registers. 
+	 * Pointers are 16-bits, so arguments are in r25:r24 and r23:22
+	 * 
+	 * [r18-r27, r30-r31]: You may use them freely in assembler subroutines.
+	 *     The caller is responsible for saving and restoring.
+	 * [r2-r17, r28-r29]: Calling C subroutines leaves them unchanged.
+	 *     Assembler subroutines are responsible for saving and restoring these registers.
+	 * [r0, r1]: Fixed registers. Never allocated by gcc for local data.
 	 */
-		"push 		r6			\n\t"
-		"push 		r7			\n\t"
-		"push 		r8			\n\t"
-		"push 		r9			\n\t"
-		"push 		r10			\n\t"
-		"push 		r11			\n\t"
-		"push 		r12			\n\t"
-		"push 		r13			\n\t"
-		"push 		r14			\n\t"
-		"push 		r15			\n\t"
-		"push 		r16			\n\t"
-		"push 		r17			\n\t"
-		"push 		r28			\n\t"
-		"push 		r29			\n\t"
+		"push        r6         \n\t"
+		"push        r7         \n\t"
+		"push        r8         \n\t"
+		"push        r9         \n\t"
+		"push        r10        \n\t"
+		"push        r11        \n\t"
+		"push        r12        \n\t"
+		"push        r13        \n\t"
+		"push        r14        \n\t"
+		"push        r15        \n\t"
+		"push        r16        \n\t"
+		"push        r17        \n\t"
+		"push        r28        \n\t"
+		"push        r29        \n\t"
 		// load plain text
 		//                s0  s1  s2  s3
 		//                s4  s5  s6  s7
 		// Cipher State   s8  s9  s10 s11
 		//                s12 s13 s14 s15
-		"ld 		r8, 		x+			\n\t"
-		"ld 		r9, 		x+			\n\t"
-		"ld 		r10, 		x+			\n\t"
-		"ld 		r11, 		x+			\n\t"
-		"ld 		r12, 		x+			\n\t"
-		"ld 		r13, 		x+			\n\t"
-		"ld 		r14, 		x+			\n\t"
-		"ld 		r15, 		x+			\n\t"
-		"ld 		r16, 		x+			\n\t"
-		"ld 		r17, 		x+			\n\t"
-		"ld 		r18, 		x+			\n\t"
-		"ld 		r19, 		x+			\n\t"
-		"ld 		r20, 		x+			\n\t"
-		"ld 		r21, 		x+			\n\t"
-		"ld 		r22, 		x+			\n\t"
-		"ld 		r23, 		x			\n\t"
+		"ld          r8,           x+        \n\t"
+		"ld          r9,           x+        \n\t"
+		"ld          r10,          x+        \n\t"
+		"ld          r11,          x+        \n\t"
+		"ld          r12,          x+        \n\t"
+		"ld          r13,          x+        \n\t"
+		"ld          r14,          x+        \n\t"
+		"ld          r15,          x+        \n\t"
+		"ld          r16,          x+        \n\t"
+		"ld          r17,          x+        \n\t"
+		"ld          r18,          x+        \n\t"
+		"ld          r19,          x+        \n\t"
+		"ld          r20,          x+        \n\t"
+		"ld          r21,          x+        \n\t"
+		"ld          r22,          x+        \n\t"
+		"ld          r23,          x         \n\t"
 		// set currentRound
-		"ldi 		r24,		40			\n\t"
+		"ldi         r24,          40        \n\t"
 		// used for const 0x02
-		"ldi 		r25,		0x02		\n\t"
-		"ldi 		r29, 		hi8(SBOX_INV)\n\t"
+		"ldi         r25,          0x02      \n\t"
+		"ldi         r29,          hi8(SBOX_INV)\n\t"
 		// encryption
-	"dec_loop:								\n\t"
+	"dec_loop:                               \n\t"
 		// mix column
 		// eor s0, s12
 		// eor s12, s4
@@ -108,120 +117,120 @@ void Decrypt(uint8_t *block, uint8_t *roundKeys)
 		// Cipher State   s12 s13 s14 s15
 		//                s0  s1  s2  s3
 		// first column
-		"eor		r8,			r20			\n\t"
-		"eor		r20,		r12			\n\t"
-		"eor		r16,		r20			\n\t"
+		"eor         r8,          r20        \n\t"
+		"eor         r20,         r12        \n\t"
+		"eor         r16,         r20        \n\t"
 		// second column
-		"eor		r9,			r21			\n\t"
-		"eor		r21,		r13			\n\t"
-		"eor		r17,		r21			\n\t"
+		"eor         r9,          r21        \n\t"
+		"eor         r21,         r13        \n\t"
+		"eor         r17,         r21        \n\t"
 		// third column
-		"eor		r10,		r22			\n\t"
-		"eor		r22,		r14			\n\t"
-		"eor		r18,		r22			\n\t"
+		"eor         r10,         r22        \n\t"
+		"eor         r22,         r14        \n\t"
+		"eor         r18,         r22        \n\t"
 		// fourth column
-		"eor		r11,		r23			\n\t"
-		"eor		r23,		r15			\n\t"
-		"eor		r19,		r23			\n\t"
-	    // shift row, add_round_const_round_key
+		"eor         r11,         r23        \n\t"
+		"eor         r23,         r15        \n\t"
+		"eor         r19,         r23        \n\t"
+		// shift row, add_round_const_round_key
 		//                s4  s5  s6  s7
 		//                s9  s10 s11 s8
 		// Cipher State   s14 s15 s12 s13
 		//                s3  s0  s1  s2
-	    "ld			r6,			z+			\n\t"
-		"eor 		r12,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r13,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r14,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r15,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r17,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r18,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r19,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r16,		r6			\n\t"
-		"eor		r22,		r25			\n\t"
+		"ld          r6,          z+         \n\t"
+		"eor         r12,         r6         \n\t"
+		"ld          r6,          z+         \n\t"
+		"eor         r13,         r6         \n\t"
+		"ld          r6,          z+         \n\t"
+		"eor         r14,         r6         \n\t"
+		"ld          r6,          z+         \n\t"
+		"eor         r15,         r6         \n\t"
+		"ld          r6,          z+         \n\t"
+		"eor         r17,         r6         \n\t"
+		"ld          r6,          z+         \n\t"
+		"eor         r18,         r6         \n\t"
+		"ld          r6,          z+         \n\t"
+		"eor         r19,         r6         \n\t"
+		"ld          r6,          z+         \n\t"
+		"eor         r16,         r6         \n\t"
+		"eor         r22,         r25        \n\t"
 		// shift_row_with_sub_column
 		//                s4  s5  s6  s7       r12 r13 r14 r15
 		//                s9  s10 s11 s8   =   r17 r18 r19 r16
 		// Cipher State   s14 s15 s12 s13  =   r22 r23 r20 r21
 		//                s3  s0  s1  s2       r11 r8  r9  r10
-		"movw		r6,			r8			\n\t"
-		"mov		r28,		r12			\n\t"
-		"ld			r8,			y			\n\t"
-		"mov		r28,		r17			\n\t"
-		"ld			r12,		y			\n\t"
-		"mov		r28,		r23			\n\t"
-		"ld			r17,		y			\n\t"
-		"mov		r28,		r10			\n\t"
-		"ld			r23,		y			\n\t"
-		"mov		r28,		r14			\n\t"
-		"ld			r10,		y			\n\t"
-		"mov		r28,		r19			\n\t"
-		"ld			r14,		y			\n\t"
-		"mov		r28,		r21			\n\t"
-		"ld			r19,		y			\n\t"
-		"mov		r28,		r8			\n\t"
-		"ld			r21,		y			\n\t"
-		// second part
-		"mov		r28,		r13			\n\t"
-		"ld			r9,			y			\n\t"
-		"mov		r28,		r18			\n\t"
-		"ld			r13,		y			\n\t"
-		"mov		r28,		r20			\n\t"
-		"ld			r18,		y			\n\t"
-		"mov		r28,		r11			\n\t"
-		"ld			r20,		y			\n\t"
-		"mov		r28,		r15			\n\t"
-		"ld			r11,		y			\n\t"
-		"mov		r28,		r16			\n\t"
-		"ld			r15,		y			\n\t"
-		"mov		r28,		r22			\n\t"
-		"ld			r16,		y			\n\t"
-		"mov		r28,		r7			\n\t"
-		"ld			r22,		y			\n\t"
-		"dec 		r24						\n\t"
-	"brne		enc_loop					\n\t"
+		"movw        r6,          r8         \n\t"
+		"mov         r28,         r12        \n\t"
+		"ld          r8,          y          \n\t"
+		"mov         r28,         r17        \n\t"
+		"ld          r12,         y          \n\t"
+		"mov         r28,         r23        \n\t"
+		"ld          r17,         y          \n\t"
+		"mov         r28,         r10        \n\t"
+		"ld          r23,         y          \n\t"
+		"mov         r28,         r14        \n\t"
+		"ld          r10,         y          \n\t"
+		"mov         r28,         r19        \n\t"
+		"ld          r14,         y          \n\t"
+		"mov         r28,         r21        \n\t"
+		"ld          r19,         y          \n\t"
+		"mov         r28,         r8         \n\t"
+		"ld          r21,         y          \n\t"
+		// second pa  rt
+		"mov          r28,        r13        \n\t"
+		"ld          r9,          y          \n\t"
+		"mov         r28,         r18        \n\t"
+		"ld          r13,         y          \n\t"
+		"mov         r28,         r20        \n\t"
+		"ld          r18,         y          \n\t"
+		"mov         r28,         r11        \n\t"
+		"ld          r20,         y          \n\t"
+		"mov         r28,         r15        \n\t"
+		"ld          r11,         y          \n\t"
+		"mov         r28,         r16        \n\t"
+		"ld          r15,         y          \n\t"
+		"mov         r28,         r22        \n\t"
+		"ld          r16,         y          \n\t"
+		"mov         r28,         r7         \n\t"
+		"ld          r22,         y          \n\t"
+		"dec         r24                     \n\t"
+	"brne        enc_loop        \n\t"
 		//                s0  s1  s2  s3       r8  r9  r10 r11
 		//                s4  s5  s6  s7   =   r12 r13 r14 r15
 		// Cipher State   s8  s9  s10 s11  =   r16 r17 r18 r19
 		//                s12 s13 s14 s15      r20 r21 r22 r23
 		// store cipher text
-		"st 		x-,			r23			\n\t"
-		"st 		x-,			r22			\n\t"
-		"st 		x-,			r21			\n\t"
-		"st 		x-,			r20			\n\t"
-		"st 		x-,			r19			\n\t"
-		"st 		x-,			r18			\n\t"
-		"st 		x-,			r17			\n\t"
-		"st 		x-,			r16			\n\t"
-		"st 		x-,			r15			\n\t"
-		"st 		x-,			r14			\n\t"
-		"st 		x-,			r13			\n\t"
-		"st 		x-,			r12			\n\t"
-		"st 		x-,			r11			\n\t"
-		"st 		x-,			r10			\n\t"
-		"st 		x-,			r9			\n\t"
-		"st 		x,			r8			\n\t"
+		"st          x-,          r23        \n\t"
+		"st          x-,          r22        \n\t"
+		"st          x-,          r21        \n\t"
+		"st          x-,          r20        \n\t"
+		"st          x-,          r19        \n\t"
+		"st          x-,          r18        \n\t"
+		"st          x-,          r17        \n\t"
+		"st          x-,          r16        \n\t"
+		"st          x-,          r15        \n\t"
+		"st          x-,          r14        \n\t"
+		"st          x-,          r13        \n\t"
+		"st          x-,          r12        \n\t"
+		"st          x-,          r11        \n\t"
+		"st          x-,          r10        \n\t"
+		"st          x-,          r9         \n\t"
+		"st          x,           r8         \n\t"
 		// --------------------------------------
-		"pop 		r29			\n\t"
-		"pop 		r28			\n\t"
-		"pop 		r17			\n\t"
-		"pop 		r16			\n\t"
-		"pop 		r15			\n\t"
-		"pop 		r14			\n\t"
-		"pop 		r13			\n\t"
-		"pop 		r12			\n\t"
-		"pop 		r11			\n\t"
-		"pop 		r10			\n\t"
-		"pop 		r9			\n\t"
-		"pop 		r8			\n\t"
-		"pop 		r7			\n\t"
-		"pop		r6			\n\t"
+		"pop         r29        \n\t"
+		"pop         r28        \n\t"
+		"pop         r17        \n\t"
+		"pop         r16        \n\t"
+		"pop         r15        \n\t"
+		"pop         r14        \n\t"
+		"pop         r13        \n\t"
+		"pop         r12        \n\t"
+		"pop         r11        \n\t"
+		"pop         r10        \n\t"
+		"pop         r9         \n\t"
+		"pop         r8         \n\t"
+		"pop         r7         \n\t"
+		"pop         r6         \n\t"
 	:
 	: [block] "x" (block), [roundKeys] "z" (roundKeys), [SBOX_INV] "" (SBOX_INV));
 }
--- encryption_key_schedule.c
+++ encryption_key_schedule.c
@@ -1,4 +1,10 @@
 /*
+ * SKINNY-128-128
+ * @Time 2016
+ * @Author luopeng(luopeng@iie.ac.cn)
+ */
+
+/*
  *
  * University of Luxembourg
  * Laboratory of Algorithmics, Cryptology and Security (LACS)
@@ -35,117 +41,117 @@
 void RunEncryptionKeySchedule(uint8_t *key, uint8_t *roundKeys)
 {
 	/*--------------------------------------*/
-	/* r5-r6	: tk0, tk4					*/
-	/* r7-r22	: master keys				*/
-	/* r23 		: loop control				*/
-	/* r24-r25 	: temp use					*/
-	/* r26-r27	: X points to plain text	*/
-	/* r30-r31	: Z points to roundKeys		*/
+	/* r5-r6   : tk0, tk4                   */
+	/* r7-r22  : master keys                */
+	/* r23     : loop control               */
+	/* r24-r25 : temp use                   */
+	/* r26-r27 : X points to plain text     */
+	/* r30-r31 : Z points to roundKeys      */
 	/* -------------------------------------*/
 	asm volatile(
-		"push 		r5			\n\t"
-		"push 		r6			\n\t"
-		"push 		r7			\n\t"
-		"push 		r8			\n\t"
-		"push 		r9			\n\t"
-		"push 		r10			\n\t"
-		"push 		r11			\n\t"
-		"push 		r12			\n\t"
-		"push 		r13			\n\t"
-		"push 		r14			\n\t"
-		"push 		r15			\n\t"
-		"push 		r16			\n\t"
-		"push 		r17			\n\t"
-		"push 		r28			\n\t"
-		"push 		r29			\n\t"
-		"movw		r26,		r24		\n\t"
-		"ldi		r23,		40		\n\t"
+		"push         r5         \n\t"
+		"push         r6         \n\t"
+		"push         r7         \n\t"
+		"push         r8         \n\t"
+		"push         r9         \n\t"
+		"push         r10        \n\t"
+		"push         r11        \n\t"
+		"push         r12        \n\t"
+		"push         r13        \n\t"
+		"push         r14        \n\t"
+		"push         r15        \n\t"
+		"push         r16        \n\t"
+		"push         r17        \n\t"
+		"push         r28        \n\t"
+		"push         r29        \n\t"
+		"movw         r26,        r24       \n\t"
+		"ldi          r23,        40        \n\t"
 		// load_keys
 		// r7  r8  r9  r10
 		// r11 r12 r13 r14
 		// r15 r16 r17 r18
 		// r19 r20 r21 r22
-		"ld			r7,			x+		\n\t"
-		"ld			r8,			x+		\n\t"
-		"ld			r9,			x+		\n\t"
-		"ld			r10,		x+		\n\t"
-		"ld			r11,		x+		\n\t"
-		"ld			r12,		x+		\n\t"
-		"ld			r13,		x+		\n\t"
-		"ld			r14,		x+		\n\t"
-		"ld			r15,		x+		\n\t"
-		"ld			r16,		x+		\n\t"
-		"ld			r17,		x+		\n\t"
-		"ld			r18,		x+		\n\t"
-		"ld			r19,		x+		\n\t"
-		"ld			r20,		x+		\n\t"
-		"ld			r21,		x+		\n\t"
-		"ld			r22,		x+		\n\t"
-		"movw		r26,		r22		\n\t"
-		"ldi		r30,		lo8(RC)	\n\t"
-		"ldi		r31,		hi8(Rc)	\n\t"
-	"key_schedule_start:				\n\t"
+		"ld           r7,         x+        \n\t"
+		"ld           r8,         x+        \n\t"
+		"ld           r9,         x+        \n\t"
+		"ld           r10,        x+        \n\t"
+		"ld           r11,        x+        \n\t"
+		"ld           r12,        x+        \n\t"
+		"ld           r13,        x+        \n\t"
+		"ld           r14,        x+        \n\t"
+		"ld           r15,        x+        \n\t"
+		"ld           r16,        x+        \n\t"
+		"ld           r17,        x+        \n\t"
+		"ld           r18,        x+        \n\t"
+		"ld           r19,        x+        \n\t"
+		"ld           r20,        x+        \n\t"
+		"ld           r21,        x+        \n\t"
+		"ld           r22,        x+        \n\t"
+		"movw         r26,        r22       \n\t"
+		"ldi          r30,        lo8(RC)   \n\t"
+		"ldi          r31,        hi8(Rc)   \n\t"
+	"key_schedule_start:                    \n\t"
 		// add_round_const
-		"lpm		r24,		z+		\n\t"
-		"mov		r25,		r24		\n\t"
-		"andi		r25,		0x0f	\n\t"
-		"mov		r5,			r7		\n\t"
-		"eor 		r5,			r25		\n\t"
-		"andi		r24,		0x30	\n\t"
-		"swap		r24					\n\t"
-		"mov		r6,			r11		\n\t"
-		"eor 		r6,			r24		\n\t"
+		"lpm          r24,        z+        \n\t"
+		"mov          r25,        r24       \n\t"
+		"andi         r25,        0x0f      \n\t"
+		"mov          r5,         r7        \n\t"
+		"eor          r5,         r25       \n\t"
+		"andi         r24,        0x30      \n\t"
+		"swap         r24                   \n\t"
+		"mov          r6,         r11       \n\t"
+		"eor          r6,         r24       \n\t"
 		// store_round_keys
-		"st			x+,			r5		\n\t"
-		"st			x+,			r8		\n\t"
-		"st			x+,			r9		\n\t"
-		"st			x+,			r10		\n\t"
-		"st			x+,			r6		\n\t"
-		"st			x+,			r12		\n\t"
-		"st			x+,			r13		\n\t"
-		"st			x+,			r14		\n\t"
-		"dec		r23					\n\t"
-		"breq		key_schedule_exit	\n\t"
+		"st           x+,         r5        \n\t"
+		"st           x+,         r8        \n\t"
+		"st           x+,         r9        \n\t"
+		"st           x+,         r10       \n\t"
+		"st           x+,         r6        \n\t"
+		"st           x+,         r12       \n\t"
+		"st           x+,         r13       \n\t"
+		"st           x+,         r14       \n\t"
+		"dec          r23                   \n\t"
+		"breq         key_schedule_exit     \n\t"
 		// 0  1  2  3         9  15 8  13
 		// 4  5  6  7         10 14 12 11
 		// 8  9  10 11 -----> 0  1  2  3
 		// 12 13 14 15        4  5  6  7
-		"mov		r24,		r7		\n\t"
-		"mov		r7,			r16		\n\t"
-		"mov		r16,		r8		\n\t"
-		"mov		r8,			r22		\n\t"
-		"mov		r22,		r14		\n\t"
-		"mov		r14,		r18		\n\t"
-		"mov		r18,		r10		\n\t"
-		"mov		r10,		r20		\n\t"
-		"mov		r20,		r12		\n\t"
-		"mov		r12,		r21		\n\t"
-		"mov		r21,		r13		\n\t"
-		"mov		r13,		r19		\n\t"
-		"mov		r19,		r11		\n\t"
-		"mov		r11,		r17		\n\t"
-		"mov		r17,		r9		\n\t"
-		"mov		r9,			r15		\n\t"
-		"mov		r15,		r24		\n\t"
-		"rjmp		key_schedule_start	\n\t"
-	"key_schedule_exit:					\n\t"
-		"pop 		r29			\n\t"
-		"pop 		r28			\n\t"
-		"pop 		r17			\n\t"
-		"pop 		r16			\n\t"
-		"pop 		r15			\n\t"
-		"pop 		r14			\n\t"
-		"pop 		r13			\n\t"
-		"pop 		r12			\n\t"
-		"pop 		r11			\n\t"
-		"pop 		r10			\n\t"
-		"pop 		r9			\n\t"
-		"pop 		r8			\n\t"
-		"pop 		r7			\n\t"
-		"pop		r6			\n\t"
-		"pop		r5			\n\t"
-	:
-	: [key] "" (key), [roundKeys] "" (roundKeys), [RC] "" (RC));
+		"mov          r24,        r7        \n\t"
+		"mov          r7,         r16       \n\t"
+		"mov          r16,        r8        \n\t"
+		"mov          r8,         r22       \n\t"
+		"mov          r22,        r14       \n\t"
+		"mov          r14,        r18       \n\t"
+		"mov          r18,        r10       \n\t"
+		"mov          r10,        r20       \n\t"
+		"mov          r20,        r12       \n\t"
+		"mov          r12,        r21       \n\t"
+		"mov          r21,        r13       \n\t"
+		"mov          r13,        r19       \n\t"
+		"mov          r19,        r11       \n\t"
+		"mov          r11,        r17       \n\t"
+		"mov          r17,        r9        \n\t"
+		"mov          r9,         r15       \n\t"
+		"mov          r15,        r24       \n\t"
+	    "rjmp         key_schedule_start    \n\t"
+	"key_schedule_exit:                     \n\t"
+		"pop          r29         \n\t"
+		"pop          r28         \n\t"
+		"pop          r17         \n\t"
+		"pop          r16         \n\t"
+		"pop          r15         \n\t"
+		"pop          r14         \n\t"
+		"pop          r13         \n\t"
+		"pop          r12         \n\t"
+		"pop          r11         \n\t"
+		"pop          r10         \n\t"
+		"pop          r9          \n\t"
+		"pop          r8          \n\t"
+		"pop          r7          \n\t"
+		"pop          r6          \n\t"
+		"pop          r5          \n\t"
+		:
+		: [key] "" (key), [roundKeys] "" (roundKeys), [RC] "" (RC));
 }
 
 #else
