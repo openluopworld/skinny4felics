bcc44e2d778a2e11781d31073f8cec8b5bce1309
openluopworld <luopengxq@gmail.com>
2016-12-27 16:57:44 +0800

    add decryption

--- decrypt.c
+++ decrypt.c
@@ -32,7 +32,7 @@
 #include "constants.h"
 
 #ifdef AVR
-void Encrypt(uint8_t *block, uint8_t *roundKeys)
+void Decrypt(uint8_t *block, uint8_t *roundKeys)
 {
 	/*--------------------------------------*/
 	/* r6-r7	: temp use					*/
@@ -72,10 +72,10 @@ void Encrypt(uint8_t *block, uint8_t *roundKeys)
 		"push 		r28			\n\t"
 		"push 		r29			\n\t"
 		// load plain text
-		// s13 s14 s15 s12
-		// s0  s1  s2  s3
-		// s7  s4  s5  s6
-		// s10 s11 s8  s9
+		//                s0  s1  s2  s3
+		//                s4  s5  s6  s7
+		// Cipher State   s8  s9  s10 s11
+		//                s12 s13 s14 s15
 		"ld 		r21, 		x+			\n\t"
 		"ld 		r22, 		x+			\n\t"
 		"ld 		r23, 		x+			\n\t"
@@ -96,9 +96,55 @@ void Encrypt(uint8_t *block, uint8_t *roundKeys)
 		"ldi 		r24,		40			\n\t"
 		// used for const 0x02
 		"ldi 		r25,		0x02		\n\t"
-		"ldi 		r29, 		hi8(SBOX)	\n\t"
+		"ldi 		r29, 		hi8(SBOX_INV)\n\t"
 		// encryption
-	"enc_loop:								\n\t"
+	"dec_loop:								\n\t"
+		// mix column
+		// eor s0, s12
+		// eor s12, s4
+		// eor s8, s12
+		//                s4  s5  s6  s7
+		//                s8  s9  s10 s11
+		// Cipher State   s12 s13 s14 s15
+		//                s0  s1  s2  s3
+		// first column
+		"eor		r8,			r20			\n\t"
+		"eor		r20,		r12			\n\t"
+		"eor		r16,		r20			\n\t"
+		// second column
+		"eor		r9,			r21			\n\t"
+		"eor		r21,		r13			\n\t"
+		"eor		r17,		r21			\n\t"
+		// third column
+		"eor		r10,		r22			\n\t"
+		"eor		r22,		r14			\n\t"
+		"eor		r18,		r22			\n\t"
+		// fourth column
+		"eor		r11,		r23			\n\t"
+		"eor		r23,		r15			\n\t"
+		"eor		r19,		r23			\n\t"
+	    // add_round_const_round_key
+		//                s4  s5  s6  s7
+		//                s9  s10 s11 s8
+		// Cipher State   s14 s15 s12 s13
+		//                s3  s0  s1  s2
+	    "ld			r6,			z+			\n\t"
+		"eor 		r12,		r6			\n\t"
+		"ld			r6,			z+			\n\t"
+		"eor 		r13,		r6			\n\t"
+		"ld			r6,			z+			\n\t"
+		"eor 		r14,		r6			\n\t"
+		"ld			r6,			z+			\n\t"
+		"eor 		r15,		r6			\n\t"
+		"ld			r6,			z+			\n\t"
+		"eor 		r17,		r6			\n\t"
+		"ld			r6,			z+			\n\t"
+		"eor 		r18,		r6			\n\t"
+		"ld			r6,			z+			\n\t"
+		"eor 		r19,		r6			\n\t"
+		"ld			r6,			z+			\n\t"
+		"eor 		r16,		r6			\n\t"
+		"eor		r22,		r25			\n\t"
 		// shift_row_with_sub_column
 		// first part t0 and t1 are pairs.
 		"movw		r6,			r8			\n\t"
@@ -135,41 +181,6 @@ void Encrypt(uint8_t *block, uint8_t *roundKeys)
 		"ld			r18,		y			\n\t"
 		"mov		r28,		r7			\n\t"
 		"ld			r13,		y			\n\t"
-	    // add_round_const_round_key
-	    "ld			r6,			z+			\n\t"
-		"eor 		r8,			r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r9,			r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r10,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r11,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r12,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r13,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r14,		r6			\n\t"
-		"ld			r6,			z+			\n\t"
-		"eor 		r15,		r6			\n\t"
-		"eor		r16,		r25			\n\t"
-		// mix column
-		// first column
-		"eor		r15,		r18			\n\t"
-		"eor		r18,		r8			\n\t"
-		"eor		r21,		r18			\n\t"
-		// second column
-		"eor		r12,		r19			\n\t"
-		"eor		r19,		r9			\n\t"
-		"eor		r22,		r19			\n\t"
-		// third column
-		"eor		r13,		r16			\n\t"
-		"eor		r16,		r10			\n\t"
-		"eor		r23,		r16			\n\t"
-		// ourth column
-		"eor		r14,		r17			\n\t"
-		"eor		r17,		r11			\n\t"
-		"eor		r20,		r17			\n\t"
 		"dec 		r24						\n\t"
 	"brne		enc_loop					\n\t"
 		// store cipher text
@@ -206,13 +217,12 @@ void Encrypt(uint8_t *block, uint8_t *roundKeys)
 		"pop 		r7			\n\t"
 		"pop		r6			\n\t"
 	:
-	: [block] "x" (block), [roundKeys] "z" (roundKeys), [SBOX] "" (SBOX));
+	: [block] "x" (block), [roundKeys] "z" (roundKeys), [SBOX_INV] "" (SBOX_INV));
 }
 
 #else
-void Encrypt(uint8_t *block, uint8_t *roundKeys)
+void Decrypt(uint8_t *block, uint8_t *roundKeys)
 {
-	/* Add here the cipher encryption implementation */
+	/* Add here the cipher decryption implementation */
 }
-
 #endif
